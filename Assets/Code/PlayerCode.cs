c4abfc6478056e2cb096b4f13b0c14096ac0248a
Kevin Chen <kc3585@nyu.edu>
2022-03-21 12:54:06 +0000

    Prof Demo 2: add combat/gun

--- Assets/Code/PlayerCode.cs
+++ Assets/Code/PlayerCode.cs
@@ -11,39 +11,67 @@ public class PlayerCode : MonoBehaviour
 
     public GameObject bulletPrefab;
     public Transform spawnPoint;
+    public Transform gun;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         _navAgent = GetComponent<NavMeshAgent>();
         mainCam = Camera.main;
         // StartCoroutine(GoRandomPoint());
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetMouseButtonDown(0)) {
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             RaycastHit hit;
-            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)) {
+            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200))
+            {
                 _navAgent.destination = hit.point;
             }
         }
 
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(1))
+        {
+            lookMouse();
+
             GameObject newBullet = Instantiate(bulletPrefab, spawnPoint.position, transform.rotation);
-            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
+            newBullet.GetComponent<Rigidbody>().AddForce(gun.forward * bulletForce);
         }
     }
 
-    IEnumerator GoRandomPoint() {
-        while (true) {
+    void FixedUpdate()
+    {
+        lookMouse();
+    }
+
+    private void lookMouse()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200))
+        {
+            Vector3 target = hit.point;
+            target.y = spawnPoint.position.y;
+            gun.LookAt(target);
+        }
+    }
+
+    IEnumerator GoRandomPoint()
+    {
+        while (true)
+        {
             yield return new WaitForSeconds(1);
             Vector3 point = new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2));
             _navAgent.destination = point;
         }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Key")) {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Key"))
+        {
             PublicVars.keyNum += 1;
             Destroy(other.gameObject);
         }
