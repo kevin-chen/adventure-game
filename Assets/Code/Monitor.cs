f02c12405b38a4e42504e360294efc6daf5d39ce
DrPeachy <p1067838263@gmail.com>
2022-03-25 13:42:38 -0400

    Update Monitor.cs

--- Assets/Code/Monitor.cs
+++ Assets/Code/Monitor.cs
@@ -6,8 +6,8 @@ public class Monitor : MonoBehaviour
 {
     public float oscillateSpeed = 1;
 
-    public Vector3 from = new Vector3(0f, -60f, 0);
-    public Vector3 to = new Vector3(0f, 60f, 0);
+    public Vector3 from = new Vector3(0f, 30f, 0);
+    public Vector3 to = new Vector3(0f, 150f, 0);
     void Start()
     {
 
@@ -16,7 +16,6 @@ public class Monitor : MonoBehaviour
     private void Update() {
         float pingpong = (Mathf.Sin(Time.time * oscillateSpeed * Mathf.PI * 2.0f) + 1.0f) / 2.0f;
 
-        //float mapped = oscillate(Time.unscaledTime, oscillateSpeed, 200);
         transform.eulerAngles = Vector3.Lerp(from, to, pingpong);
     }
 
