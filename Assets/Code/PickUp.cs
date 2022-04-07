1f93cc8309d03fa5ab3f2f553f14b948e02edd81
DrPeachy <p1067838263@gmail.com>
2022-04-07 18:11:28 -0400

    pickup

--- Assets/Code/PickUp.cs
+++ Assets/Code/PickUp.cs
@@ -6,106 +6,111 @@ public class PickUp : MonoBehaviour
 {
     public float throwForce = 600;
     Vector3 objectPos;
-    Vector3 startPos;
-    float distance;
-
-    public bool canHold = true;
-    public GameObject item;
-    public GameObject tempParent;
+    public float distance;
+    public GameObject bomb;
+    public Transform bombPos;
     public bool isHolding = false;
+    Rigidbody _bombRig;
 
     AudioSource aud;
     public AudioClip explosion;
-
-    public float timeUntilExplosion = 30;
-
+    public float timeUntilExplosion = 15;
     public TextMeshProUGUI countdown;
+    public Camera mainCam;
 
-    // set tempParent to Bomb position under playerModel
 
     void Start() {
         aud = GetComponent<AudioSource>();
-        startPos = item.transform.position;
+        bomb = GameObject.FindGameObjectWithTag("bomb");
+        _bombRig = bomb.GetComponent<Rigidbody>();
+        
     } 
 
- // Update is called once per frame
-    void Update () {
-        Timer();
-
-        countdown.text = "0:" + (int)(timeUntilExplosion);
-
-        distance = Vector3.Distance (item.transform.position, tempParent.transform.position);
-        if (distance >= 5f) 
-        {
-            isHolding = false;
+    private void FixedUpdate() {
+        if(bomb){
+            Timer();
         }
+    }
+    void Update () {
+        
+        if(bomb){
 
-  //Check if isholding
-        if (isHolding) {
-            item.GetComponent<Rigidbody> ().velocity = Vector3.zero;
-            item.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
-            item.transform.SetParent (tempParent.transform);
-            item.transform.position = tempParent.transform.position;
-            //StartCoroutine(Explode());
-
-            // if (timeUntilExplosion > 0) {
-            //     timeUntilExplosion -= Time.deltaTime;    
-            //     countdown.text = (int)(timeUntilExplosion / 10) + ":" + (timeUntilExplosion % 10 * 100);
-            // }
-            // else {
-            //     Explode();
-            //     countdown.text = "0:00";
-            //     aud.PlayOneShot(explosion);
-            // }
-
-            if (Input.GetKeyDown(KeyCode.E)) {
-                item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+            distance = Vector3.Distance(bomb.transform.position, bombPos.position);
+            if (distance >= 5f) 
+            {
                 isHolding = false;
-                //StopAllCoroutines();
-                //aud.PlayOneShot(explosion);
-                //StartCoroutine(Explode_After_Throw());
+            }
+
+    //Check if isholding
+            if (isHolding) {
+                if (Input.GetKeyDown(KeyCode.E)) {
+
+                    _bombRig.isKinematic = false;
+                    bomb.transform.SetParent(null);
+                    bomb.GetComponent<Rigidbody>().AddForce(bombPos.transform.forward * throwForce);
+                    isHolding = false;
+
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0) && distance <= 5)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200))
+                {
+                    if(hit.collider.CompareTag("bomb")){
+                        PublicVars.isPickedUp = true;
+                        _bombRig.isKinematic = true;
+                        isHolding = true;
+                        bomb.transform.position = bombPos.position;
+                        bomb.transform.parent = transform;
+                    }
+                }
             }
         }
-        else 
-        {
-            objectPos = item.transform.position;
-            item.transform.SetParent(null);
-            item.GetComponent<Rigidbody>().useGravity = true;
-            item.transform.position = objectPos;
-        }
+
     }
 
-    void OnMouseDown() 
-    {
-        if (distance <= 5f)
-        {
-            isHolding = true;
-            PublicVars.isPickedUp = true;
-            item.GetComponent<Rigidbody> ().useGravity = false;
-            item.GetComponent<Rigidbody> ().detectCollisions = true;
 
-        }
-    }
+    // void OnMouseDown() 
+    // {
+    //     if (distance <= 5f)
+    //     {
+    //         if(gameObject.name == "bomb"){
+    //             print("click");
+    //             _bombRig = gameObject.GetComponent<Rigidbody>();
+    //             _bombRig.isKinematic = true;
+    //             isHolding = true;
+    //             PublicVars.isPickedUp = true;
+    //             gameObject.transform.parent = transform;
+    //             gameObject.transform.position = transform.Find("Bombposition").position;
+    //             // gameObject.GetComponent<Rigidbody>().useGravity = false;
+    //             // gameObject.GetComponent<Rigidbody>().detectCollisions = true;
+    //         }
+
+    //     }
+    // }
  
 //this will later be modified to decrease the player's health if the
 //player is one of the nearby objects
 
     void Explode() {
-        Collider[] nearby = Physics.OverlapSphere(item.transform.position, 25);
-        //GameObject[] nearby = Physics.OverlapSphere(item.transform.position, 25);
+        Collider[] nearby = Physics.OverlapSphere(bomb.transform.position, 25);
+        //GameObject[] nearby = Physics.OverlapSphere(bomb.transform.position, 25);
         foreach (var obj in nearby) {
             if (obj.CompareTag("ThingToDestroy")) {
                 Destroy(obj.gameObject);
             }
         }
-        Destroy(item.gameObject);
+        Destroy(bomb.gameObject);
     }
 
     void Timer(){
         if(PublicVars.isPickedUp){
             if (timeUntilExplosion > 0) {
                 timeUntilExplosion -= Time.deltaTime;    
-                countdown.text = (int)(timeUntilExplosion / 10) + ":" + (timeUntilExplosion % 10 * 100);
+                countdown.text = Mathf.Round(timeUntilExplosion) + ":" + (Mathf.Round(timeUntilExplosion * 100) % 100);
+
             }
             else {
                 Explode();
@@ -114,18 +119,4 @@ public class PickUp : MonoBehaviour
             }
         }
     }
-    // IEnumerator Explode_After_Throw() {
-    //     yield return new WaitForSeconds(0.5f);
-    //     Collider[] nearby = Physics.OverlapSphere(item.transform.position, 25);
-    //     //GameObject[] nearby = Physics.OverlapSphere(item.transform.position, 25);
-    //     foreach (var obj in nearby) {
-    //         if (!obj.gameObject.CompareTag("Player") && !obj.CompareTag("Floor")) {
-    //             Destroy(obj.gameObject);
-    //         }
-    //     }
-    //     //aud.PlayOneShot(explosion);
-    //     Destroy(item.gameObject);
-    // }
-
-
 }
